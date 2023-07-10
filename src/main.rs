72edbd62ad513a35d8a806b3560b1a1b17bfaab1
Cedric VINCENT <cedric.vincent@st.com>
2023-07-10 16:25:59 +0200

    Minor changes everywhere.

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,46 @@
-fn main () -> Result<(), std::io::Error> {
+//mod error;
+//use error::Error;
+
+fn main () {
 	let dev_name = "eth1";
-	let dt_name  = find_dt_name(dev_name)?.unwrap();
-	let gpio     = find_gpio(&dt_name)?.unwrap();
-	let (base, offset) = find_address(&gpio)?.unwrap();
+
+	let dt_name = find_dt_name(dev_name).unwrap(); // TODO: ?
+	let gpio    = find_gpio(&dt_name).unwrap();    // TODO: ?
+	let address = find_address(&gpio).unwrap();    // TODO: ?
 
 	// TODO: log::info instead of println
 	println!("device named \"{dev_name}\" is known as \"{dt_name}\" in device-tree");
 	println!("↳ its RGMII GTX clock is connected to GPIO {gpio}");
-	println!("  ↳ its delay can be accessed at address {base:#x} (bits {}-{}) in /dev/mem", offset, offset + 4);
-
-	Ok(())
+	println!("  ↳ its delay can be accessed at address {address} in /dev/mem");
 }
 
-#[derive(Debug)]
-struct Gpio {
-	bank: char,
-	line: u8,
-}
+fn find_dt_name (dev_name: &str) -> Result<String, std::io::Error> {
+	use std::io::BufRead;
 
-impl std::fmt::Display for Gpio {
-	fn fmt(&self, formatter: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-		write!(formatter, "{}{}", self.bank, self.line)
-	}
-}
+	let path   = format!("/sys/class/net/{dev_name}/device/uevent");
+	let handle = std::fs::File::open(path)?;
+	let reader = std::io::BufReader::new(handle);
 
-fn find_address (gpio: &Gpio) -> Result<Option<(usize, u8)>, std::io::Error> {
-	let path = format!("/sys/firmware/devicetree/base/__symbols__/gpio{}", gpio.bank.to_lowercase());
-	let path = std::fs::read_to_string(path)?;
-	let path = path.trim_end_matches('\0');
+	for line in reader.lines().flatten() {
+		let mut tokens = line.split('=');
 
-	match path.split('@').last() {
-		None          => Ok(None),
-		Some(address) => {
-			match usize::from_str_radix(address, 16) {
-				Ok(address) => Ok(Some((address + 0x40, gpio.line * 4))),
-				Err(_)      => Err(std::io::Error::from(std::io::ErrorKind::InvalidData)) // TODO: user friendly error
+		if tokens.next() == Some("OF_NAME") {
+			return match tokens.next() {
+				Some(token) => Ok(String::from(token)),
+				None        => Err(std::io::Error::from(std::io::ErrorKind::NotFound)),
 			}
 		}
 	}
+
+	Err(std::io::Error::from(std::io::ErrorKind::NotFound))
 }
 
-fn find_gpio (dt_name: &str) -> Result<Option<Gpio>, std::io::Error> {
+fn find_gpio (dt_name: &str) -> Result<Gpio, std::io::Error> {
 	use std::io::BufRead;
 
-	let entries = std::fs::read_dir("/sys/kernel/debug/pinctrl/")?; // TODO: user friendly error
+	let entries = std::fs::read_dir("/sys/kernel/debug/pinctrl/")?;
 	for entry in entries {
-		let entry = entry?; // TODO: user friendly error
+		let entry = entry?;
 
 		let file_name = entry.file_name();
 		let file_name = file_name.to_string_lossy();
@@ -59,11 +54,10 @@ fn find_gpio (dt_name: &str) -> Result<Option<Gpio>, std::io::Error> {
 		let mut path = entry.path();
 		path.push("pinconf-pins");
 
-		let handle = std::fs::File::open(path)?; // TODO: user friendly error
+		let handle = std::fs::File::open(path)?;
 		let reader = std::io::BufReader::new(handle);
 		let needle = format!("{}_RGMII_GTX_CLK", dt_name.to_uppercase());
 
-		// TODO: user friendly error
 		let error = || { std::io::Error::from(std::io::ErrorKind::InvalidData) };
 
 		for line in reader.lines().flatten() {
@@ -82,33 +76,55 @@ fn find_gpio (dt_name: &str) -> Result<Option<Gpio>, std::io::Error> {
 			let line  = tokens.as_str().parse::<u8>().ok();
 
 			if magic != Some('P') || bank.is_none() || line.is_none() {
-				continue; // TODO: user friendly error
+				continue;
 			}
 
-			return Ok(Some(Gpio {
+			return Ok(Gpio {
 				bank: bank.unwrap(),
 				line: line.unwrap(),
-			}));
+			});
 		}
 	}
 
-	Ok(None)
+	Err(std::io::Error::from(std::io::ErrorKind::NotFound))
 }
 
-fn find_dt_name (dev_name: &str) -> Result<Option<String>, std::io::Error> {
-	use std::io::BufRead;
+fn find_address (gpio: &Gpio) -> Result<Address, std::io::Error> {
+	let path = format!("/sys/firmware/devicetree/base/__symbols__/gpio{}", gpio.bank.to_lowercase());
+	let path = std::fs::read_to_string(path)?;
+	let path = path.trim_end_matches('\0');
 
-	let path   = format!("/sys/class/net/{dev_name}/device/uevent");
-	let handle = std::fs::File::open(path)?; // TODO: user friendly error
-	let reader = std::io::BufReader::new(handle);
-
-	for line in reader.lines().flatten() {
-		let mut tokens = line.split('=');
-
-		if tokens.next() == Some("OF_NAME") {
-			return Ok(tokens.next().map(String::from));
+	match path.split('@').last() {
+		None          => Err(std::io::Error::from(std::io::ErrorKind::NotFound)),
+		Some(address) => {
+			match usize::from_str_radix(address, 16) {
+				Ok(address) => Ok(Address { base: address + 0x40, offset: gpio.line * 4 }),
+				Err(_)      => Err(std::io::Error::from(std::io::ErrorKind::InvalidData)),
+			}
 		}
 	}
+}
 
-	Ok(None)
+#[derive(Debug)]
+struct Gpio {
+	bank: char,
+	line: u8,
+}
+
+#[derive(Debug)]
+struct Address {
+	base:   usize,
+	offset: u8,
+}
+
+impl std::fmt::Display for Gpio {
+	fn fmt(&self, formatter: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+		write!(formatter, "{}{}", self.bank, self.line)
+	}
+}
+
+impl std::fmt::Display for Address {
+	fn fmt(&self, formatter: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+		write!(formatter, "{:#x} (bits {}-{})", self.base, self.offset, self.offset + 4)
+	}
 }
