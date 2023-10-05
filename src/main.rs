6c2bcd2f5a64c11a6045ca36025e25d77542e1dd
Cedric VINCENT <cedric.vincent@st.com>
2023-10-05 07:23:57 +0200

    This program is copyright STMicroelectronics using a BSD-3 clauses license.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+// Copyright 2023 STMicroelectronics
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the
+//    distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 mod error;
 mod clock_delay;
 mod benchmark;
@@ -35,6 +65,40 @@ fn main2 (options: Options) -> Result<(), Error> {
 			timeout }                    => benchmark::perform(&device, &url, speed_low_limit, timeout)?,
 		Command::Set { device, clock_delay } => clock_delay::access(&device, Some(clock_delay), true)?,
 		Command::Get { device }              => clock_delay::access(&device, None, true)?,
+
+		Command::License { } => {
+			println!("\n\
+				Copyright 2023 STMicroelectronics\n\
+				\n\
+				Redistribution and use in source and binary forms, with or without\n\
+				modification, are permitted provided that the following conditions are\n\
+				met:\n\
+				\n\
+				1. Redistributions of source code must retain the above copyright\n\
+				   notice, this list of conditions and the following disclaimer.\n\
+				\n\
+				2. Redistributions in binary form must reproduce the above copyright\n\
+				   notice, this list of conditions and the following disclaimer in the\n\
+				   documentation and/or other materials provided with the\n\
+				   distribution.\n\
+				\n\
+				3. Neither the name of the copyright holder nor the names of its\n\
+				   contributors may be used to endorse or promote products derived\n\
+				   from this software without specific prior written permission.\n\
+				\n\
+				THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS\n\
+				'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT\n\
+				LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR\n\
+				A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT\n\
+				HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,\n\
+				SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT\n\
+				LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,\n\
+				DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY\n\
+				THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n\
+				(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n\
+				OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\
+			");
+		}
 	}
 
 	Ok(())
@@ -88,7 +152,10 @@ enum Command {
 		/// Device name
 		#[clap(short, long)]
 		device: String,
-	}
+	},
+
+	/// Print license & copyright for this software
+	License { }
 }
 
 fn speed_low_limit_parser (value: &str) -> Result<Byte, String> {
